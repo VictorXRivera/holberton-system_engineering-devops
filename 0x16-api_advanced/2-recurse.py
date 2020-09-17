9ede7a492afd3c8bc1a941dba96bc55c11fcd60d
VictorXRivera <victorxrivera.professional@gmail.com>
2020-09-17 23:19:31 +0000

    Recursive function to query Reddit API

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
-Fuction that queries the Reddit API and prints
-the titles of the first 10 hot posts listed for a given subreddit.
+Recursive function that queries the Reddit API and returns a list
+containing the titles of all hot articles for a given subreddit.
+If no results are found for the given subreddit, the function should
+return None.
 """
 import requests
 
 
-def top_ten(subreddit):
-    """Top ten function"""
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Recurse"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
